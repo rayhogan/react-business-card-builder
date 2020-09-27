62ed6d52f40203b824366196810574e57380622b
Ray Hogan <rayhogan@gmail.com>
2020-09-27 21:03:47 +0100

    npm pages assets

---
+++ docs/precache-manifest.87277243daef602d046f7181939a40e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8513f97bf24e4fe7768ea092d5016ece",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61bdc707b7f0bf2c6846",
+    "url": "/static/css/main.766a4788.chunk.css"
+  },
+  {
+    "revision": "d428b256b02e3922ea4a",
+    "url": "/static/js/2.feee8762.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.feee8762.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61bdc707b7f0bf2c6846",
+    "url": "/static/js/main.a757fe1a.chunk.js"
+  },
+  {
+    "revision": "9b6bc4d998db7d9f1a3b",
+    "url": "/static/js/runtime-main.b9cdc1ae.js"
+  }
+]);
